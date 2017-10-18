177b18ea00b3af5f69daefc7ca7efab22fd760d3
Joey Phan <joseph.phan@whitehatsec.com>
2017-10-17 23:23:34 -0700

    update coen280 sql table queries

--- SqlSnippets/coen280.sql
+++ SqlSnippets/coen280.sql
@@ -1,14 +1,17 @@
 CREATE TABLE YelpUser (
+    YelpID int,
     BirthDate varchar(255),
     BirthPlace varchar(255),
     EmailID int,
     ProfilePic int,
-    YelpID int,
     Gender varchar(255),
     FirstName varchar(255),
     LastName varchar(255),
+    ReviewID int,
 
     PRIMARY KEY(YelpUserID)
+    FOREIGN KEY(ReviewID) REFERENCES Review
+
 );
 
 CREATE TABLE ListOfFriends(
@@ -18,66 +21,58 @@ CREATE TABLE ListOfFriends(
     ViewRecommendedReviews int, -- Acts as a boolean
     VotedReviewAsPositive varchar(255), -- Acts as a boolean -- positive = funny/cool/helpful
     MarkedHelpful int, -- Acts as a boolean
-
 );
---
+
 
 CREATE TABLE Review (
-	ReviewID int,
-	Rating int,
-	PublicDate varchar(255),
+    ReviewID int,
+    BusinessID int,
+    YelpUserID int,
+    Rating int,
+    PublicDate varchar(255),
     Recommeneded varchar(255),
-    ContentType varchar(255),
+    ContentType varchar(255),   -- decides whether content is a text or photo
     ContentText varchar(255),
-    ContentPhoto varchar(255)
+    ContentPhoto varchar(255),
+
+
+    PRIMARY KEY (ReviewID),
+    FOREIGN KEY (BusinessID) REFERENCES Business,
+    FOREIGN KEY (YelpUserID) REFERENCES YelpUser
 );
 
 CREATE TABLE Business (
-  Name varchar(255),
-  BusinessID int,
-  PhotoNumber varchar(255),
-  Address varchar(255),
-  reviewCount int,
-  BusinessCategoryID int,
-  RecommendedReviewID int,  -- how does this represent one to many??
-  HelpfulPhotoID int, -- how does this represent one to many??
-
-  PRIMARY KEY (BusinessID),
-  FOREIGN KEY (BusinessCategoryID) REFERENCES BusinessCategory,
-  FOREIGN KEY (RecommendedReviewID) REFERENCES RecommendedReview,
-);
-
-CREATE TABLE RecommendedReview(
-   ReviewID int,
-   BusinessID int,
-
-   PRIMARY KEY (ReviewID),
-   FOREIGN KEY (YelpUserID) REFERENCES YelpUser,
-   FOREIGN KEY (BusinessID) REFERENCES Business,
-);
-
-CREATE TABLE BusinessCategory(
     Name varchar(255),
-    BusinessCategoryID ID,
-    BusinessCategoryType varchar(255)
-);
-
-CREATE TABLE HelpfulPhoto(
-    PhotoID int,
     BusinessID int,
+    PhotoNumber varchar(255),
+    Address varchar(255),
+    reviewCount int,
+    BusinessCategoryID int,
+    ReviewID int,
+    PhotoID int, -- how does this represent one to many??
 
-    FOREIGN KEY (BusinessID) REFERENCES Business,
+    PRIMARY KEY (BusinessID),
+    FOREIGN KEY (CategoryID) REFERENCES Category,
+    FOREIGN KEY (ReviewID) REFERENCES Review,
     FOREIGN KEY (PhotoID) REFERENCES Photo,
 );
 
-CREATE TABLE Photo(
+CREATE TABLE BusinessCategory(
+    BusinessCategoryID int,
+    Name varchar(255),
+    BusinessID int,
+    BusinessCategoryType varchar(255),
+
+    PRIMARY KEY (BusinessCategoryID)
+    FOREIGN KEY (BusinessID) REFERENCES Business
+);
+
+CREATE TABLE BusinessPhoto(
     PhotoID int,
     PhotoDescription varchar (255),
     Location varchar (255),
     BusinessID int,
-    YelpUserID int,
 
     PRIMARY KEY (PhotoID),
-    FOREIGN KEY (BusinessID) REFERENCES Business,
-    FOREIGN KEY (YelpUserID) REFERENCES YelpUser
+    FOREIGN KEY (BusinessID) REFERENCES Business
 );
